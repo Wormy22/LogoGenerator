250e1ce3be125941b6e8f96f9f44fbdac807a93e
tcritchlow <Wormy22@users.noreply.github.com>
2018-07-25 21:07:47 +0100

    Started creating a common file

--- gen_altran.py
+++ gen_altran.py
@@ -1,49 +1,34 @@
 from PIL import Image, ImageFont, ImageDraw
 
+import common
+
 OUTPUT = 'altran_out.png'
 FONT = "resources/Altran.ttf"
 
-def interpolate(f_co, t_co, interval):
-    det_co = [(t - f) / interval for f, t in zip(f_co, t_co)]
-    for i in range(interval):
-        yield [round(f + det * i) for f, det in zip(f_co, det_co)]
+LEFT_COLOUR = (19, 8, 100)
+RIGHT_COLOUR = (70, 157, 236)
 
 
-def create_gradient(width, height):
-    gradient = Image.new('RGBA', (width, height), color=0)
-    grad_draw = ImageDraw.Draw(gradient)
-
-    left_colour = (19, 8, 100)
-    right_colour = (70, 157, 236)
-    for i, colour in enumerate(interpolate(left_colour, right_colour, width)):
-        grad_draw.line([(i, 0), (i, height)], tuple(colour), width=1)
-
-    return gradient
-
-user_text = "frasier"
+user_text = "frasssier"
 
 # Create temporary image to determine length of text
 temp_image = Image.new("RGBA", (1, 1), (0, 0, 0))
 usr_font = ImageFont.truetype(FONT, 500)
 
 text_width, text_height = ImageDraw.Draw(temp_image).textsize(user_text, usr_font)
-out_width = text_width + 20
-out_height = text_height + 50
+output_size = (text_width + 20, text_height + 50)
 
 # Create background
-bg = Image.new('RGBA', (out_width, out_height), (255, 255, 255))
+bg = Image.new('RGBA', output_size, (255, 255, 255))
 
 # Create gradient
-gradient = create_gradient(out_width, out_height)
+gradient = common.create_horizontal_gradient(LEFT_COLOUR, RIGHT_COLOUR, output_size)
 
 # Apply gradient to text
-mask = Image.new('1', (out_width, out_height))
-mask_draw = ImageDraw.Draw(mask)
-mask_draw.text((10, 0), user_text, font=usr_font, fill='#ffffff')
-bg.paste(gradient, (0,0), mask)
+common.mask_text(bg, user_text, usr_font, gradient, output_size)
 
 # Resample and save output
-bg.thumbnail((int(out_width/2), int(out_height/2)), Image.ANTIALIAS)
+common.resample_image(bg, output_size)
 bg.save(OUTPUT, 'PNG')
 
 # TODO - add weird bar thing
--- gen_frasier.py
+++ gen_frasier.py
@@ -1,24 +1,11 @@
 from PIL import Image, ImageFont, ImageDraw
 
+import common
+
 FONT = "resources/florentine regular.ttf"
 
-
-def interpolate(f_co, t_co, interval):
-    det_co = [(t - f) / interval for f, t in zip(f_co, t_co)]
-    for i in range(interval):
-        yield [round(f + det * i) for f, det in zip(f_co, det_co)]
-
-
-def create_gradient(width, height):
-    gradient = Image.new('RGBA', (width, height), color=0)
-    grad_draw = ImageDraw.Draw(gradient)
-
-    top_colour = (255, 199, 0)
-    bottom_colour = (254, 254, 200)
-    for i, colour in enumerate(interpolate(top_colour, bottom_colour, height)):
-        grad_draw.line([(0, i), (width, i)], tuple(colour), width=1)
-
-    return gradient
+TOP_COLOUR = (255, 199, 0)
+BOTTOM_COLOUR = (254, 254, 200)
 
 user_text = "altran".upper()
 # Todo - Make first bigger
@@ -28,23 +15,19 @@ temp_image = Image.new("RGBA", (1, 1), (0, 0, 0))
 usr_font = ImageFont.truetype(FONT, 500)
 
 text_width, text_height = ImageDraw.Draw(temp_image).textsize(user_text, usr_font)
-out_width = text_width + 20
-out_height = text_height + 30
+output_size = (text_width + 20, text_height + 30)
 
 # Create background
-bg = Image.new('RGBA', (out_width, out_height), (0, 0, 0))
+bg = Image.new('RGBA', output_size, (0, 0, 0))
 
 # Create gradient
-gradient = create_gradient(out_width, out_height)
+gradient = common.create_vertical_gradient(TOP_COLOUR, BOTTOM_COLOUR, output_size)
 
 # Apply gradient to text
-mask = Image.new('1', (out_width, out_height))
-mask_draw = ImageDraw.Draw(mask)
-mask_draw.text((0, 0), user_text, font=usr_font, fill='#ffffff')
-bg.paste(gradient, (0,0), mask)
+common.mask_text(bg, user_text, usr_font, gradient, output_size)
 
 # Save output
-bg.thumbnail((int(out_width/2), int(out_height/2)), Image.ANTIALIAS)
+common.resample_image(bg, output_size)
 bg.save('frasier_out.png', 'PNG')
 
 # TODO - skyline
